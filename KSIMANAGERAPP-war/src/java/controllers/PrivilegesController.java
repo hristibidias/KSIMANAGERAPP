153a0213710e4c7d7b09d745942160ec6e6e7e06
Hristi <hrististoikov@gmail.com>
2017-09-28 23:45:00 +0200

    implémentation de l'authentification OK implémentation de la gestion privilèges

--- KSIMANAGERAPP-war/src/java/controllers/PrivilegesController.java
+++ KSIMANAGERAPP-war/src/java/controllers/PrivilegesController.java
@@ -5,12 +5,19 @@
  */
 package controllers;
 
+import entities.Menu;
+import entities.Personnel;
 import entities.Privileges;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.event.ActionEvent;
+import org.primefaces.component.commandbutton.CommandButton;
+import org.primefaces.context.RequestContext;
+import sessions.MenuFacadeLocal;
+import sessions.PersonnelFacadeLocal;
 import sessions.PrivilegesFacadeLocal;
 
 /**
@@ -25,6 +32,16 @@ public class PrivilegesController implements Serializable  {
     private Privileges privileges = new Privileges();
     private String operation;
     private String msg;
+    @EJB
+    private PersonnelFacadeLocal personnelFacade;
+    private List<Personnel> listPersonnel = new ArrayList<>();
+    private Personnel personnel = new Personnel();
+    private Integer idPersonnel;
+    @EJB
+    private MenuFacadeLocal menuFacade;
+    private List<Menu> listMenu = new ArrayList<>();
+    private Menu menu = new Menu();
+    private Integer idMenu;
     /**
      * Creates a new instance of PrivilegesController
      */
@@ -35,8 +52,105 @@ public class PrivilegesController implements Serializable  {
     public void init(){
         listPrivileges.clear();
         listPrivileges.addAll(privilegesFacade.findAll());
+        chargeListMenu();
+        chargeListPersonnel();
+    }
+    
+    public void chargeListMenu(){
+        listMenu.clear();
+        listMenu.addAll(menuFacade.findAll());
+    }
+    public void chargeListPersonnel(){
+        listPersonnel.clear();
+        listPersonnel.addAll(personnelFacade.findAll());
     }
 
+    public void action(ActionEvent e) {
+        CommandButton btn = (CommandButton) e.getSource();
+        operation = btn.getWidgetVar();
+        msg = "";
+    }
+
+    public void prepareCreate(ActionEvent e) {
+        privileges = new Privileges();
+        msg = "";
+        action(e);
+    }
+    
+    public void savePrivileges() {
+        try {
+            //privilegesFacade.create(privileges);
+            privilegesFacade.create(privileges);
+            msg = "Opération effectuée avec succès!";
+            RequestContext.getCurrentInstance().execute("PF('wv_privileges').hide()");
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg = "Echec de l'opération!";
+        } finally {
+            init();
+        }
+    }
+
+    public void modifyPrivileges() {
+        try {
+            privilegesFacade.edit(privileges);
+            msg = "Successful operation!";
+            RequestContext.getCurrentInstance().execute("PF('wv_privileges').hide()");
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg = "Failure!";
+        } finally {
+            init();
+        }
+    }
+
+    public void deletePrivileges() {
+        try {
+            privilegesFacade.remove(privileges);
+            msg = "Successful operation!";
+            RequestContext.getCurrentInstance().execute("PF('wv_privileges').hide()");
+        } catch (Exception e) {
+            e.printStackTrace();
+            msg = "Failure!";
+        } finally {
+            init();
+        }
+    }
+
+    public void persist() {
+        switch (operation) {
+            case "addPrivileges":
+                savePrivileges();
+                break;
+            case "modifyPrivileges":
+                modifyPrivileges();
+                break;
+            case "deletePrivileges":
+                deletePrivileges();
+                break;
+        }
+    }
+
+//    public String imprimer() {
+//        try {
+//            JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(listUtilisateur);
+//            String reportPath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("resources/etats/listUtilisateur.jasper");
+//            Map parameters = new HashMap();
+//            parameters.put("USER", connectedUser);
+//            parameters.put("REPORT_LOCALE", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+//            JasperPrint jasperPrint = fillReport(reportPath, parameters, beanCollectionDataSource);
+//            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+//            httpServletResponse.addHeader("Content-disposition", "attachment; filename=listUtilisateur.pdf");
+//            ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
+//            exportReportToPdfStream(jasperPrint, servletOutputStream);
+//            FacesContext.getCurrentInstance().responseComplete();
+//            //----------------------------------------------
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return FacesContext.getCurrentInstance().getExternalContext().getRequestPathInfo() + "?faces-redirect=true";
+//    }
+    
     public PrivilegesFacadeLocal getPrivilegesFacade() {
         return privilegesFacade;
     }
@@ -76,5 +190,70 @@ public class PrivilegesController implements Serializable  {
     public void setMsg(String msg) {
         this.msg = msg;
     }
+
+    public PersonnelFacadeLocal getPersonnelFacade() {
+        return personnelFacade;
+    }
+
+    public void setPersonnelFacade(PersonnelFacadeLocal personnelFacade) {
+        this.personnelFacade = personnelFacade;
+    }
+
+    public List<Personnel> getListPersonnel() {
+        return listPersonnel;
+    }
+
+    public void setListPersonnel(List<Personnel> listPersonnel) {
+        this.listPersonnel = listPersonnel;
+    }
+
+    public Personnel getPersonnel() {
+        return personnel;
+    }
+
+    public void setPersonnel(Personnel personnel) {
+        this.personnel = personnel;
+    }
+
+    public Integer getIdPersonnel() {
+        return idPersonnel;
+    }
+
+    public void setIdPersonnel(Integer idPersonnel) {
+        this.idPersonnel = idPersonnel;
+    }
+
+    public MenuFacadeLocal getMenuFacade() {
+        return menuFacade;
+    }
+
+    public void setMenuFacade(MenuFacadeLocal menuFacade) {
+        this.menuFacade = menuFacade;
+    }
+
+    public List<Menu> getListMenu() {
+        return listMenu;
+    }
+
+    public void setListMenu(List<Menu> listMenu) {
+        this.listMenu = listMenu;
+    }
+    
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+
+    public Integer getIdMenu() {
+        return idMenu;
+    }
+
+    public void setIdMenu(Integer idMenu) {
+        this.idMenu = idMenu;
+    }
+    
     
 }
--- KSIMANAGERAPP-war/src/java/controllers/SessionController.java
+++ KSIMANAGERAPP-war/src/java/controllers/SessionController.java
@@ -5,8 +5,12 @@
  */
 package controllers;
 
+import entities.Personnel;
 import java.io.Serializable;
+import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+import sessions.PersonnelFacadeLocal;
 
 /**
  *
@@ -14,11 +18,52 @@ import javax.faces.context.FacesContext;
  */
 public class SessionController implements Serializable{
 
+    @EJB
+    private PersonnelFacadeLocal personnelFacade;
+    private Personnel currentUser = new Personnel();   
     private String langue = "fr";
     /**
      * Creates a new instance of SessionController
      */
     public SessionController() {
+    }
+    
+    public void watchOut() {
+        try {
+            if (!FacesContext.getCurrentInstance().getExternalContext().getSessionMap().containsKey("currentUser")) {
+                ((FacesContext.getCurrentInstance()).getExternalContext()).redirect("authenticate.xhtml?faces-redirect=true");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String authenticate() {
+        try {
+            currentUser = personnelFacade.findByLoginMdp(currentUser.getLogin(), ((Integer) currentUser.getPassword().hashCode()).toString());
+            if (currentUser != null) {
+                FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("currentUser", currentUser);
+                return "index.xhtml?faces-redirect=true";
+            } else {
+                currentUser = new Personnel();
+                return "authenticate.xhtml?faces-redirect=true";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            currentUser = new Personnel();
+            return "authenticate.xhtml?faces-redirect=true";
+        }
+    }
+
+    public String logOut() {
+        try {
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().remove("currentUser");
+            ((HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false)).invalidate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return "authenticate.xhtml?faces-redirect=true";
+        }
     }
     
     public String english() {
@@ -38,5 +83,22 @@ public class SessionController implements Serializable{
     public void setLangue(String langue) {
         this.langue = langue;
     }
+
+    public PersonnelFacadeLocal getPersonnelFacade() {
+        return personnelFacade;
+    }
+
+    public void setPersonnelFacade(PersonnelFacadeLocal personnelFacade) {
+        this.personnelFacade = personnelFacade;
+    }
+
+    public Personnel getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(Personnel currentUser) {
+        this.currentUser = currentUser;
+    }
       
+    
 }
